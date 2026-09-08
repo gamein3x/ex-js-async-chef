98722d4475e0c7cb0759771bf6c1ab7478974eb6
Ivan Caldarella <gamein3x@gmail.com>
2026-09-08 19:07:01 +0200

    Inizio esercizio Compleanno dello chef

---
+++ index.js
@@ -0,0 +1,43 @@
+/* In questo esercizio, utilizzerai async/await per creare la funzione getChefBirthday(id).
+ Questa funzione accetta un id di una ricetta e deve:
+-Recuperare la ricetta da https://dummyjson.com/recipes/{id}
+-Estrarre la proprietà userId dalla ricetta
+-Usare userId per ottenere le informazioni dello chef da https://dummyjson.com/users/{userId}
+-Restituire la data di nascita dello chef
+Note del docente
+Scrivi la funzione getChefBirthday(id), che deve:
+Essere asincrona (async).
+Utilizzare await per chiamare le API.
+Restituire una Promise con la data di nascita dello chef.
+Gestire gli errori con try/catch
+Esempio di utilizzo
+"
+getChefBirthday(1)
+  .then(birthday => console.log("Data di nascita dello chef:", birthday))
+  .catch(error => console.error("Errore:", error.message));
+"
+Esempio di output:
+> Data di nascita dello chef: 1990-06-15 */
+
+async function getChefBirthday(id) {
+    const recipe = await fetch(`https://dummyjson.com/recipes/${id}`);
+    const recipeRes = await recipe.json();
+    const user = await recipeRes.userId;
+
+    const chef = await fetch(`https://dummyjson.com/users/${user}`);
+    const chefRes = await chef.json();
+
+    return console.log(chefRes);
+}
+
+
+getChefBirthday(2);
+
+
+/* 🎯 Bonus 1
+Attualmente, se la prima richiesta non trova una ricetta, la seconda richiesta potrebbe comunque essere eseguita
+ causando errori a cascata. */
+
+/* Modifica getChefBirthday(id) per intercettare eventuali errori prima di fare la seconda richiesta.
+🎯 Bonus 2
+Utilizza la libreria dayjs per formattare la data di nascita nel formato giorno/mese/anno. */
